63d9b701244b48cdeab841640f5b5dc57c177199
matiss-dumeranss <matiss.skola@gmail.com>
2023-11-24 14:30:50 +0200

    Izpildīts 5.uzdevums (pilnīgs)

--- 5.uzdevums.py
+++ 5.uzdevums.py
@@ -1,10 +1,19 @@
-a=int(input("Ievadi pulksteņa laiku Veselos skaitļos..."))
-#def sveiciens(a=int(input("Ievadi pulksteņa laiku Veselos skaitļos..."))):
-if a>6 and a<12:
+#a=int(input("Ievadi pulksteņa laiku Veselos skaitļos..."))
+
+#if a>6 and a<12:
+ #   print("Labrīt!")
+#elif a>12 and a<18:
+ #   print("Labdien!")
+#elif a>18 and a<24:
+#    print("Labvakar!")
+
+a=str(input("Norādi dienas laiku (ja pulkstens ir no 6 līdz 12 raksti rits,ja pulkstens ir no 12 līdz 18 raksti diena un ja pulkstens ir no 18 līdz 24 raksti vakars )..."))
+
+if a=="rits":
     print("Labrīt!")
-elif a>12 and a<18:
+elif a=="diena":
     print("Labdien!")
-elif a>18 and a<24:
+elif a=="vakars":
     print("Labvakar!")
     
 
