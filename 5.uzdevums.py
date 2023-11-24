acf8eebf039f031eb3200597bcb0105add77c76d
matiss-dumeranss <matiss.skola@gmail.com>
2023-11-24 14:03:38 +0200

    Izveidoju 5.uzdevumu ( nepilnīgs)

---
+++ 5.uzdevums.py
@@ -0,0 +1,10 @@
+a=int(input("Ievadi pulksteņa laiku Veselos skaitļos..."))
+#def sveiciens(a=int(input("Ievadi pulksteņa laiku Veselos skaitļos..."))):
+if a>6 and a<12:
+    print("Labrīt!")
+elif a>12 and a<18:
+    print("Labdien!")
+elif a>18 and a<24:
+    print("Labvakar!")
+    
+
